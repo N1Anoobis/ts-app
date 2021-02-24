dcc05fe6e79e5e1805b65c3833eea68f23136c45
N1Anoobis <mrbond84@hotmail.com>
2021-02-24 17:51:47 +0100

    Add registration base components globaly

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseCard from "./components/UI/BaseCard.vue";
+import BaseButton from "./components/UI/BaseButton.vue";
+
+Vue.component("base-card", BaseCard);
+Vue.component("base-button", BaseButton);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
